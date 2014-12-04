7df904c3d077997a7e0e4b68d97ad0da1e436ef6
Romain Semler <romain.semler@gmail.com>
2014-12-05 00:04:04 +0100

    Update CNode.hxx

--- CNode.hxx
+++ CNode.hxx
@@ -1,57 +1,49 @@
 /********************************
- * @author	Romain Semler, Yon Vidal, 
- * 		Jérémy Rossignol, Kévin Rozotte
- * 		Manon Saillard
- * 
- * @file	CNode.hxx
- * 
- * @date	28/11/2014
- * 
- * @update	01/12/2014
- * 
- * @synopsis	Implémentation des fonctions 
- * 		définies dans CNode.h
- * *******************************/
+* @author   Romain Semler, Yon Vidal,
+*           Jérémy Rossignol, Kévin Rozotte
+*           Manon Saillard
+*
+* @file     CNode.hxx
+*
+* @date     28/11/2014
+*
+* @update   05/12/2014
+*
+* @synopsis Implémentation des fonctions
+*           définies dans CNode.h
+* *******************************/
 
 #ifndef __CNODE_HXX__
 #define __CNODE_HXX__
 
-#include "CNode.h"
 #define TEMPLATE template <class T>
 
+#include "CNode.h"
 #include <iostream>
 #include <memory>
 
 TEMPLATE
 CNode<T>::CNode ()
 {
-	
 }
 
 TEMPLATE
-CNode<T>::CNode (const T &valeur = T (), const std::shared_ptr <CNode <T> > precedent = 0, const std::shared_ptr <CNode <T> > suivant = 0)
+CNode<T>::CNode (const T &valeur = T (), const std::shared_ptr <CNode <T> > precedent /*= 0*/, const std::shared_ptr <CNode <T> > suivant /*= 0*/)
 {
-	m_Data = valeur;
-	m_Prev = precedent;
-	m_Next = suivant;
+    m_Data = valeur;
+    m_Prev = precedent;
+    m_Next = suivant;
 }
 
 TEMPLATE
 CNode<T>::~CNode()
 {
-
 }
 
 TEMPLATE
-T CNode<T>::GetData()
+T CNode<T>::GetData() const
 {
     return m_Data;
-}
-
-TEMPLATE
-void CNode<T>::SetData (const T &valeur)
-{
-    m_Data = valeur;
 }
 
 TEMPLATE
@@ -61,9 +53,26 @@ std::shared_ptr <CNode <T> > CNode<T>::GetNextNode()
 }
 
 TEMPLATE
+std::shared_ptr <CNode <T> > CNode<T>::GetPrevNode()
+{
+    return m_Prev;
+}
+
+TEMPLATE
+void CNode<T>::SetData (const T &valeur)
+{
+    m_Data = valeur;
+}
+
+TEMPLATE
 void CNode<T>::SetNextNode(std::shared_ptr <CNode <T> > * next)
 {
     m_Next = next;
 }
 
-#endif // CNODE_HXX_INCLUDED
+void CNode<T>::SetPrevNode(std::shared_ptr <CNode <T> > * prev)
+{
+    m_Pext = prev;
+}
+
+#endif // __CNODE_HXX__
